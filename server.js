9d6654b1d4903e4a94203d724d3fe501cc14f5a7
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-07-01 19:39:50 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const MongoStore = require("connect-mongo");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3001;
+const handlebars = require("express-handlebars");
+const hbs = handlebars.create({});
+const { v4: uuid } = require("uuid");
+
+const app = express();
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/avatar-legends-db"
+);
+mongoose.set("debug", true);
+
+const hours = 1;
+app.use(
+  session({
+    genid: () => uuid(),
+    secret: "Jotaro is best Jojo",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: hours * 3600000,
+    },
+    store: MongoStore.create({
+      mongoUrl: "mongodb://localhost:27017/avatar-legends-db",
+    }),
+  })
+);
+
+app.engine("handlebars", hbs.engine);
+app.set("views engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => console.log(`Connected to Port: ${PORT}`));
